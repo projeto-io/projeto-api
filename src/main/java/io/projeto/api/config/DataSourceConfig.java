3e257768ecef308e8bba22161e81927bff0d1d70
CHANMYUNG <nooheat1228@gmail.com>
2020-06-26 22:29:09 +0900

    feat: Add project configurations

---
+++ src/main/java/io/projeto/api/config/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package io.projeto.api.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "dataSource")
+    @Primary
+    @ConfigurationProperties("spring.datasource.hikari")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
