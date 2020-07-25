6e670c739b7b27aea74e794ae201c7964e70ef18
CHANMYUNG <nooheat1228@gmail.com>
2020-07-25 16:32:22 +0900

    feat: Rename project create API endpoint
    - Rename project create API endpoint from /v1/projects to /projects

--- src/main/java/io/projeto/api/project/controller/ProjectController.java
+++ src/main/java/io/projeto/api/project/controller/ProjectController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/projects")
+@RequestMapping("/projects")
 public class ProjectController {
 
     private final ProjectService projectService;
